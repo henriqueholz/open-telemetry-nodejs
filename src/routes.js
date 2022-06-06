5e4db83d96fd2e4304a7d591a0003c5afca9b69c
henriqueholz <henriqueholz@live.com>
2022-06-06 20:51:25 -0300

    Adding collector

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,22 @@
-const Router = require('koa-router')
+const Router = require("koa-router");
 
 // middlewares
-const error = require('./middlewares/error')
-const authenticated = require('./middlewares/auth')
+const error = require("./middlewares/error");
+// const authenticated = require('./middlewares/auth')
 
 // handlers
-const users = require('./handlers/users')
-const auth = require('./handlers/auth')
+const users = require("./handlers/users");
+const auth = require("./handlers/auth");
 
-const router = new Router()
+const router = new Router();
 
-router.use(error)
+router.use(error);
 
-router.get('/users', authenticated, users.getAllUsers)
-router.post('/users', authenticated, users.createUser)
+router.get("/users", users.getAllUsers);
+router.post("/users", users.createUser);
 
-router.post('/auth', auth.authenticate)
-router.post('/auth/refreshToken', auth.refreshToken)
-router.post('/auth/logout', auth.logout)
+router.post("/auth", auth.authenticate);
+router.post("/auth/refreshToken", auth.refreshToken);
+router.post("/auth/logout", auth.logout);
 
-module.exports = router
+module.exports = router;
